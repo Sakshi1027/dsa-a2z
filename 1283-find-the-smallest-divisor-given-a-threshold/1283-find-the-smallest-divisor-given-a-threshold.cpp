81584988a3729ed81c5c943e4088f016f68e150f
Sakshi A S <sakshias.cs23@rvce.edu.in>
2026-05-12 10:34:16 +0530

    17 ms (9.91%) 26 MB (79.48%) 05-12-2026 1283-find-the-smallest-divisor-given-a-threshold Medium .cpp

---
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int low=1,high=*max_element(nums.begin(),nums.end());
+        int result=high;
+        
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            long long sum=0;
+            for(int n:nums)
+            {
+                 sum+=ceil((double)n/mid);
+            }
+            if(sum<=threshold)
+            {
+                result=mid;
+                high=mid-1;
+            }
+            else 
+             low=mid+1;
+        }
+        return result;
+    }
+};
