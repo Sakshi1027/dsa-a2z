e824eab63a25fbb58ae88956bcfea11754c9ed82
Sakshi A S <sakshias.cs23@rvce.edu.in>
2026-05-26 10:19:36 +0530

    0 ms (100%) 9.6 MB (91.97%) 05-26-2026 0242-valid-anagram Easy .cpp

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int freq[26]={0};
+        int n=s.size();
+        if(s.size()!=t.size())
+         return false;
+        for(int i=0;i<n;i++)
+        {
+            freq[s[i]-'a']++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            freq[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i]!=0)
+             return false;
+        }
+        return true;
+
+        
+    }
+};
