33d43ad64b9a68ec2bc50168c9a990bfff207696
Sakshi A S <sakshias.cs23@rvce.edu.in>
2026-06-03 10:32:37 +0530

    3 ms (14.26%) 10.4 MB (5.17%) 06-03-2026 0008-string-to-integer-atoi Medium .cpp

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
+    int helper(const string s,int i,long long num,int sign)
+    {
+        if(i>s.size() || !isdigit(s[i]))
+         return (int)(sign*num);
+        num=num*10+(s[i]-'0');
+        if(sign*num>INT_MAX)
+         return INT_MAX;
+        if(sign*num<INT_MIN)
+         return INT_MIN;
+        return helper(s,i+1,num,sign);
+
+        
+    
+    }
     int myAtoi(string s) {
-        int i = 0;
-        int n = s.length();
-
-        // remove leading spaces
-        while (i < n && s[i] == ' ')
+        int i=0;
+        while(i<s.size() && s[i]==' ')
+         i++;
+        int sign=1;
+        if(i<s.size() && (s[i]=='+'||s[i]=='-'))
+        {
+            if(s[i]=='-')
+            
+             sign=-1;
             i++;
-
-        // sign
-        int sign = 1;
-
-        if (i < n && (s[i] == '+' || s[i] == '-')) {
-            if (s[i] == '-')
-                sign = -1;
-            i++;
+            
+            
         }
-
-        long long ans = 0;
-
-        // digits
-        while (i < n && isdigit(s[i])) {
-
-            ans = ans * 10 + (s[i] - '0');
-
-            // overflow check
-            if (sign * ans > INT_MAX)
-                return INT_MAX;
-
-            if (sign * ans < INT_MIN)
-                return INT_MIN;
-
-            i++;
-        }
-
-        return (int)(sign * ans);
+        return helper(s,i,0,sign);
+        
     }
 };
