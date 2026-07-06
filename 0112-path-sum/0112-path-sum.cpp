014607e282a4ad1c4da6cd205e9910fa0112d44b
Sakshi A S <sakshias.cs23@rvce.edu.in>
2026-07-06 11:41:21 +0530

    0 ms (100%) 21.5 MB (66.52%) 07-06-2026 0112-path-sum Easy .cpp

---
+++ 0112-path-sum/0112-path-sum.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool sum(TreeNode* root,int target)
+    {
+        if(root==nullptr)
+         return false;
+        if(root->left==nullptr && root->right==nullptr)
+         return target==root->val;
+        return sum(root->left,target-root->val) ||
+        sum(root->right,target-root->val);
+    }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        return sum(root,targetSum);
+        
+    }
+};
