639cbde926817cc5cb7d8622ee61a6ff5118c6dd
Sakshi A S <sakshias.cs23@rvce.edu.in>
2026-07-06 21:10:52 +0530

    0 ms (100%) 28.3 MB (9.74%) 07-06-2026 0169-majority-element Easy .cpp

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        return nums[n/2];
-        
+        unordered_map<int,int> mp;
+        for(int x:nums)
+        {
+            mp[x]++;
+        }
+        int max=0;
+        int ans=-1;
+        for(auto it:mp)
+        {
+            if(it.second>max)
+            {
+                max=it.second;
+                ans=it.first;
+            }
+        }
+        return ans;
     }
 };
