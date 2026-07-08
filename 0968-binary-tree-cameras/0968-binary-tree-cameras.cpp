40ea1e98e4c987d5b1e748caa5ef3bc60e855ddb
Sakshi A S <sakshias.cs23@rvce.edu.in>
2026-07-08 12:04:40 +0530

    0 ms (100%) 21.4 MB (37.27%) 07-08-2026 0968-binary-tree-cameras Hard .cpp

---
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int cameras=0;
+    int solve(TreeNode* root)
+    {
+        if(root==nullptr)
+         return 2;
+        int left=solve(root->left);
+        int right=solve(root->right);
+        if(left==0||right==0)
+        {
+            cameras++;
+            return 1;
+        }
+        if(left==1||right==1)
+         return 2;
+        return 0;
+    }
+    int minCameraCover(TreeNode* root) {
+        if(solve(root)==0)
+          cameras++;
+        return cameras;
+        
+    }
+};
