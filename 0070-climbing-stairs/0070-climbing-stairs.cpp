6827774191aa36386caaa5f0bd174d563ce142bf
Sakshi A S <sakshias.cs23@rvce.edu.in>
2026-08-12 12:01:01 +0530

    0 ms (100%) 8.5 MB (39.1%) 08-12-2026 0070-climbing-stairs Easy .cpp

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    int dp[46] = {0};
+    
 
     int climbStairs(int n) {
-        if(n <= 1)
-            return 1;
-
-        if(dp[n] != 0)
-            return dp[n];
-
-        return dp[n] = climbStairs(n-1) + climbStairs(n-2);
+       vector<int> dp(n+1);
+       dp[0]=1;
+       dp[1]=1;
+       for(int i=2;i<=n;i++)
+       {
+        dp[i]=dp[i-1]+dp[i-2];
+       }
+       return dp[n];
     }
 };
