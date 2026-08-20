7883f5bdbe2cb333529bd0a00ee9d166dcb23e40
Sakshi A S <sakshias.cs23@rvce.edu.in>
2026-08-20 12:20:47 +0530

    1 ms (84.73%) 15.3 MB (42.98%) 08-20-2026 0797-all-paths-from-source-to-target Medium .cpp

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> path;
+    void dfs(int node,vector<vector<int>>& graph)
+    {
+        path.push_back(node);
+        if(node==graph.size()-1)
+        {
+            ans.push_back(path);
+            path.pop_back();
+            return;
+        }
+        for(int neighbor:graph[node])
+         dfs(neighbor,graph);
+        path.pop_back();
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        dfs(0,graph);
+        return ans;
+        
+    }
+};
