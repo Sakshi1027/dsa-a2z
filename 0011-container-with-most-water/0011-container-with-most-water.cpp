088de456763b1f7eb715604f37572dd8a7228dfe
Sakshi A S <sakshias.cs23@rvce.edu.in>
2026-08-26 13:14:27 +0530

    0 ms (100%) 63 MB (11.08%) 08-26-2026 0011-container-with-most-water Medium .cpp

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l=0,r=height.size()-1;
+        int max_area=0;
+        while(l<r)
+        {
+           int area=(r-l)*min(height[l],height[r]);
+           max_area=max(area,max_area);
+           if(height[l]<height[r])
+             l++;
+            else
+             r--;
+        }
+        return max_area;
+
+        
+    }
+};
